e999c717ed0fa9295534576b6d38a713d00cf911
aldinhro <33513597+aldinhro@users.noreply.github.com>
2017-11-17 08:56:46 +0100

    User Class

---
+++ Grupp5Projekt/Grupp5Projekt/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp5Projekt
+{
+    public abstract class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public enum Privilege { admin, student, teacher }
+        public Privilege privilege { get; set; }
+
+    }
+}

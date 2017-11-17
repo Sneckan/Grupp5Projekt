2f898f352d7912caf42aa585698a8536ccda325a
jennybacklund <33482998+jennybacklund@users.noreply.github.com>
2017-11-17 11:01:52 +0100

    AddUser, RemoveUser
    Methods for add and remove user in Admin Class

---
+++ Grupp5Projekt/Grupp5Projekt/Admin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp5Projekt
+{
+    public class Admin
+    {
+        //Propertys
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Mail { get; set; }
+        public string User { get; set; }
+
+        //Constructor
+        public Admin(string name, string password, string mail)
+        {
+            Name = name;
+            Password = password;
+            Mail = mail;
+        }
+
+        //AddUser
+        public void AddUser(string name, string password, string mail)
+        {
+            Console.WriteLine("Insert name: ");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Insert password: ");
+            Password = Console.ReadLine();
+
+            Console.WriteLine("Insert e-mail adress: ");
+            Mail = Console.ReadLine();
+
+            var addUserList = new List<Registry>();
+            var listSize = new Registry(50);
+            list.Add(AddUser(Name, Password, Mail));
+        }
+
+        //public void RemoveUser()
+        //{
+        //    Console.WriteLine("Remove user number: ");
+        //    User = Console.ReadLine();
+
+        //    var list = new list<Registry>();
+        //    var listSize = new Registry(50);
+        //    list.Add(2);
+        //    list.Add(new Registry(2));
+
+        //    var index = list.IndexOf(2);
+        //    list.Remove(2);
+        //    list.RemoveAt(0);
+
+        //}
+    }
+}
---
+++ Grupp5Projekt/UnitTest/AdminTest.cs
@@ -0,0 +1,19 @@
+﻿using Grupp5Projekt;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class AdminTests
+    {
+        [Test]
+        public void AddUserShouldAddNamePasswordAndMail()
+        {
+            var user = new Admin("Kalle", "password", "mail");
+            user.AddUser();
+            Assert.AreEqual("Kalle", user.Name);
+            Assert.AreEqual("password", user.Password);
+            Assert.AreEqual("mail", user.Mail);
+        }
+    }
+}

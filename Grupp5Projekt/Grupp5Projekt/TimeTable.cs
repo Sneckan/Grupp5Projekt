9905c942e427ae38f8fe754148be07bf07f15b2d
Sneckan <paccelito@gmail.com>
2017-11-20 10:42:13 +0100

    timetableish

---
+++ Grupp5Projekt/Grupp5Projekt/TimeTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp5Projekt
+{
+  class TimeTable
+  {
+    Dictionary<Course, Dictionary<Room, DateTime>> timeTable;
+
+
+
+  }
+}

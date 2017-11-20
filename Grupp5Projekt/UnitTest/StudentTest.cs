5a77625aaa5cc122df740f07a8889b06d69fb237
Substitutional <32260164+Substitutional@users.noreply.github.com>
2017-11-20 15:00:32 +0100

    kjugvogpjdz

--- Grupp5Projekt/UnitTest/StudentTest.cs
+++ Grupp5Projekt/UnitTest/StudentTest.cs
@@ -1,4 +1,5 @@
-﻿using Grupp5Projekt;
+﻿using System.Net;
+using Grupp5Projekt;
 using NUnit.Framework;
 
 namespace UnitTest
@@ -9,7 +10,10 @@ namespace UnitTest
     [Test]
     public void CheckIfStudentsAreCreatedCorrectly()
     {
+      User.Privilege privilege = User.Privilege.student;
+      Student student = new Student("Steve Steveson", "steve@Steve.se", "Steve123", privilege);
 
+      Assert.AreEqual("Steve Steveson", student.Name);
     }
   }
 }

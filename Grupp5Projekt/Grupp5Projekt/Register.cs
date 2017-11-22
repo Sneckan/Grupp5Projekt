17afe6af30e669d3318a59c7e8b2634ae728b80f
jennybacklund <33482998+jennybacklund@users.noreply.github.com>
2017-11-22 14:03:45 +0100

    Added stuff to registry + tests
    Added stuff to registry + tests

--- Grupp5Projekt/Grupp5Projekt/Register.cs
+++ Grupp5Projekt/Grupp5Projekt/Register.cs
@@ -6,81 +6,110 @@ using System.Threading.Tasks;
 
 namespace Grupp5Projekt
 {
-    public class Register
+  public class Register
+  {
+    public List<User> Users { get; set; }
+    public List<Course> Courses { get; set; }
+    public List<Room> Rooms { get; set; }
+
+    //Constructor for Users
+    public Register(string rName, string rEmail, string rPassword)
     {
-        public List<User> Users { get; set; }
-        public List<Course> Courses { get; set; }
-        public List<Room> Rooms { get; set; }
-
-        //Constructor
-        public Register(string rName, string rEmail, string rPassword)
-        {
-            Users = new List<User>();
-        }
-
-        public Register(string rName, string rEmail, string rPassword, List<User> users)
-        {
-            Users = users;
-        }
-
-        public Register(string rName, Teacher rTeacher, DateTime rStartDate, DateTime rEndDate, int rHours)
-        {
-            Courses = new List<Course>();
-        }
-
-        public Register(string rName, Teacher rTeacher, DateTime rStartDate, DateTime rEndDate, int rHours, List<Course> course)
-        {
-            Courses = course;
-        }
-
-        public Register(string rName)
-        {
-            Rooms = new List<Room>();
-        }
-
-        public Register(string rName, List<Room> rooms)
-        {
-            Rooms = rooms;
-        }
-
-        //Add Admin
-        public void AddAdminUser(string rName, string rPassword, string rEmail)
-        {
-            Users.Add(new Admin(rName, rPassword, rEmail, User.Privilege.admin));
-        }
-
-        //Remove Admin
-
-        //Add Teacher
-        public void AddTeacherUser(string rName, string rPassword, string rEmail)
-        {
-            Users.Add(new Teacher(rName, rPassword, rEmail, User.Privilege.teacher));
-        }
-
-        //Remove Teacher
-
-        //Add Student
-        public void AddStudentUser(string rName, string rPassword, string rEmail)
-        {
-            Users.Add(new Student(rName, rPassword, rEmail, User.Privilege.student));
-        }
-
-        //Remove Student
-
-        //Add Course
-        public void AddCourse(string rName, Teacher rTeacher, DateTime rStartDate, DateTime rEndDate, int rHours)
-        {
-            Courses.Add(new Course(rName, rTeacher, rStartDate, rEndDate, rHours));
-        }
-
-        //Remove Course
-
-        //Add Room
-        public void AddRoom(string rName)
-        {
-            Rooms.Add(new Room(rName));
-        }
-
-        //Remove Room
+      Users = new List<User>();
     }
+
+    public Register(string rName, string rEmail, string rPassword, List<User> users)
+    {
+      Users = users;
+    }
+
+    //Constructor for Courses
+    public Register(string rName, Teacher rTeacher, DateTime rStartDate, DateTime rEndDate, int rHours)
+    {
+      Courses = new List<Course>();
+    }
+
+    public Register(string rName, Teacher rTeacher, DateTime rStartDate, DateTime rEndDate, int rHours, List<Course> course)
+    {
+      Courses = course;
+    }
+
+    //Constructor for Rooms
+    public Register(string rName)
+    {
+      Rooms = new List<Room>();
+    }
+
+    public Register(string rName, List<Room> rooms)
+    {
+      Rooms = rooms;
+    }
+
+    //Methods
+    //Add Admin
+    public void AddAdminUser(string rName, string rPassword, string rEmail)
+    {
+      Users.Add(new Admin(rName, rPassword, rEmail, User.Privilege.admin));
+    }
+
+    public void AddAdminUser(Admin admin)
+    {
+      Users.Add(admin);
+    }
+
+    //Remove Admin
+
+    //Add Teacher
+    public void AddTeacherUser(string rName, string rPassword, string rEmail)
+    {
+      Users.Add(new Teacher(rName, rPassword, rEmail, User.Privilege.teacher));
+    }
+
+    public void AddTeacherUser(Teacher teacher)
+    {
+      Users.Add(teacher);
+    }
+
+
+    //Remove Teacher
+
+    //Add Student
+    public void AddStudentUser(string rName, string rPassword, string rEmail)
+    {
+      Users.Add(new Student(rName, rPassword, rEmail, User.Privilege.student));
+    }
+
+    public void AddStudentUser(Student student)
+    {
+      Users.Add(student);
+    }
+
+    //Remove Student
+
+    //Add Course
+    public void AddCourse(string rName, Teacher rTeacher, DateTime rStartDate, DateTime rEndDate, int rHours)
+    {
+      Courses.Add(new Course(rName, rTeacher, rStartDate, rEndDate, rHours));
+    }
+
+    public void AddCourse (Course course)
+    {
+      Courses.Add(course);
+    }
+
+    //Remove Course
+
+    //Add Room
+    public void AddRoom(string rName)
+    {
+      Rooms.Add(new Room(rName));
+    }
+
+    public void AddRoom(Room room)
+    {
+      Rooms.Add(room);
+    }
+
+    //Remove Room
+  }
 }
--- Grupp5Projekt/UnitTest/RegisterTest.cs
+++ Grupp5Projekt/UnitTest/RegisterTest.cs
@@ -1,12 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Grupp5Projekt;
+using NUnit.Framework;
 
 namespace UnitTest
 {
-    class RegisterTest
+  [TestFixture]
+  class RegisterTest
+  {
+    [Test]
+    public void AddAdminToRegistry()
     {
+      Register register = new Register("Name", "Password", "Email");
+      register.AddAdminUser("Name", "Password", "Email");
+
+      Assert.AreEqual(register.Users.Count, 1);
+      Assert.AreEqual(register.Users[0].MyPrivilege, User.Privilege.admin);
     }
+
+    [Test]
+    public void AddTeacherToRegistry()
+    {
+      Register register = new Register("Name", "Password", "Email");
+      register.AddTeacherUser("Name", "Password", "Email");
+
+      Assert.AreEqual(register.Users.Count, 1);
+      Assert.AreEqual(register.Users[0].MyPrivilege, User.Privilege.teacher);
+    }
+
+    [Test]
+    public void AddStudentToRegistry()
+    {
+      Register register = new Register("Name", "Password", "Email");
+      register.AddStudentUser("Name", "Password", "Email");
+
+      Assert.AreEqual(register.Users.Count, 1);
+      Assert.AreEqual(register.Users[0].MyPrivilege, User.Privilege.student);
+    }
+
+    //[Test]
+    //public void AddCourseToRegistry()
+    //{
+    //  Register register = new Register("Name", "Password", "Email");
+    //  Teacher teacher = new Teacher("Name", "Email", "Password", User.Privilege.teacher);
+    //  Course course = new Course("Name", teacher, DateTime.MaxValue, DateTime.MinValue, 5);
+
+    //  register.AddCourse(course);
+
+    //  Assert.AreEqual(register.Courses.Count, 1);
+    //}
+  } 
 }
